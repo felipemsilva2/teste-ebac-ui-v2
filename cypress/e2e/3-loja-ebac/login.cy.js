78027f1ac9ffe78d1b7a7024601909244174d3c7
felipe <felipedcl@icloud.com>
2025-02-17 17:26:23 -0300

    Utilizando comandos customizados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -15,7 +15,6 @@ describe('Funcionalidade: Login', () => {
         cy.get('#username').type('felipemgl@teste.com.br')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
           .should('contain', 'Olá, felipemgl (não é felipemgl? Sair)')
     });
@@ -47,7 +46,7 @@ describe('Funcionalidade: Login', () => {
           .should('contain', 'Olá, felipemgl (não é felipemgl? Sair)')
     });
 
-    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+    it('Deve fazer login com sucesso - Usando Fixture', () => {
         cy.fixture('perfil').then((dados) => {
             cy.get('#username').type(dados.usuario,{ log:false})
             cy.get('#password').type(dados.senha,{ log:false})
@@ -56,5 +55,11 @@ describe('Funcionalidade: Login', () => {
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
               .should('contain', 'Olá, felipemgl (não é felipemgl? Sair)')
         });
+    });
+
+    it.only('Deve fazer login com sucesso - usando comandos customizados', () => {
+        cy.login('felipemgl@teste.com.br', 'teste@teste')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
+          .should('contain', 'Olá, felipemgl (não é felipemgl? Sair)')
     });
 });
